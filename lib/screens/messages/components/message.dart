501c4c8de307957e77c3a088d1a73868006b1ed0
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2021-11-09 16:44:49 +0330

    add status for messages

--- lib/screens/messages/components/message.dart
+++ lib/screens/messages/components/message.dart
@@ -45,8 +45,46 @@ class Message extends StatelessWidget {
               width: cDefaultPadding / 2,
             ),
           ],
-          messageContaint(message)
+          messageContaint(message),
+          if (message.isSender)
+            MessageStatusDot(
+              status: message.messageStatus,
+            )
         ],
+      ),
+    );
+  }
+}
+
+class MessageStatusDot extends StatelessWidget {
+  final MessageStatus status;
+  const MessageStatusDot({Key? key, required this.status}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color dotColor(MessageStatus status) {
+      switch (status) {
+        case MessageStatus.not_sent:
+          return Theme.of(context).errorColor;
+        case MessageStatus.not_view:
+          return Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.1);
+        case MessageStatus.viewed:
+          return cPrimaryColor;
+        default:
+          return Colors.transparent;
+      }
+    }
+
+    return Container(
+      margin: const EdgeInsets.only(left: cDefaultPadding / 2),
+      height: 16,
+      width: 16,
+      decoration:
+          BoxDecoration(color: dotColor(status), shape: BoxShape.circle),
+      child: Icon(
+        status == MessageStatus.not_sent ? Icons.close : Icons.done,
+        size: 10,
+        color: Theme.of(context).scaffoldBackgroundColor,
       ),
     );
   }
